8e40eb4593f211c28bf59bbd6f58c76b0806a06c
thronOne97 <81563915+thronOne97@users.noreply.github.com>
2022-03-30 17:54:37 -0600

    2.EnumType

---
+++ enum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+enum weekDays{Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+
+int main()
+{
+	enum weekDays today;
+	today = Friday;
+	printf("Day %d", today+1);
+	return 0;
+}
