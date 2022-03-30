f00922b1408be71c75161efd291d7bf9fe9b20cd
thronOne97 <81563915+thronOne97@users.noreply.github.com>
2022-03-30 17:54:10 -0600

    Hello Everyone

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //Preprocessor Command
+int main() //Ejecución principal del codigo
+{
+	/*First Hello Worl in C language*/
+	printf("Hello One"); //función que nos permite imprimir
+		return 0; //termina la ejecución del programa
+}
