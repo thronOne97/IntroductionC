a08cca1b322c6cbd5f7e84f3f61e576bd550711c
thronOne97 <81563915+thronOne97@users.noreply.github.com>
2022-03-30 18:25:22 -0600

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Variable Declarations
+int a, b, c;
+
+float f, g, h;
+
+
+
+int main()
+{
+	//Variable Initialization
+	a = 10;
+	b = 2147483647;
+
+	c = a + b;
+	printf("Value  of sum is: %d", c);
+	return 0;
+}
