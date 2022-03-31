4fab701f117c0912f6dfe8841a8a0e7b9ef146e1
thronOne97 <81563915+thronOne97@users.noreply.github.com>
2022-03-30 20:42:31 -0600

    Add files via upload

---
+++ function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int power(int base, int n);
+
+int main()
+{
+	int i;
+	for(i = 0; i < 10; i++)
+	{
+		printf("n = %d El resultado de la potencia = %d\n", i, power(2, i));
+	}
+
+	return 0;
+}
+
+int power(int base, int n)
+{
+	int i, p;
+	p = 1;
+	for(i = 1; i<= n; i++)
+	{
+		p = p * base;
+	}
+
+	return p;
+}
